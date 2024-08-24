94f34f8fa0daeb9527f8b7463b2a8eeb84e5dd54
Brian Gawalt <bgawalt@gmail.com>
2024-08-23 22:11:28 -0700

    Join game counts across off, def, kck

--- lombardotron.py
+++ lombardotron.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 import statvalues
 
@@ -14,29 +15,69 @@ _SEASON_2023 = (_OFF_2023, _DEF_2023, _KCK_2023)
 
 # TODO: move REG, POST, REG+POST to statvalues enum
 
+_PID_COLUMN = "player_id"
+_NAME_COLUMN = "player_name"
+_SEASON_TYPE_COLUMN = "season_type"
+
 class SeasonStats:
 
     def __init__(self, season: tuple[str, str, str], season_type: str):
         off_file, def_file, kck_file = season
-        with open(kck_file, 'rt') as infile:
+        kck_teams = {}  # {player id : {team : num_games}}
+        off_team = {}  # {player_id: team}
+        def_teams = {}  # {player id : {team : num_games}}
+        names = {} # {player id : name}
+        with open(kck_file, "rt") as infile:
             for row in csv.DictReader(infile):
-                if "kck_games" not in row:
-                    raise ValueError("kck_file missing kck_games")
-                for k in sorted(row.keys()):
-                    print(k)
-                break
-        player_ids = set([])
-        with open(off_file, 'rt') as infile:
+                if row[_SEASON_TYPE_COLUMN] != season_type:
+                    continue
+                pid = row[_PID_COLUMN]
+                name = row[_NAME_COLUMN]
+                if pid not in names:
+                    names[pid] = name
+                elif pid in names and names[pid] != name:
+                    raise ValueError(f'Repeat names for {pid}: {name}, {names[pid]}')
+                if pid not in kck_teams:
+                    kck_teams[pid] = {}
+                team = row["team"]
+                num_games = int(row["kck_games"])
+                kck_teams[pid][team] = kck_teams[pid].get(team, 0) + num_games
+        with open(off_file, "rt") as infile:
             for row in csv.DictReader(infile):
-                if "games" not in row:
-                    raise ValueError("off_file missing games")
-                player_ids.add(row['player_id'])
-        with open(def_file, 'rt') as infile:
+                if row[_SEASON_TYPE_COLUMN] != season_type:
+                    continue
+                pid = row[_PID_COLUMN]
+                name = row[_NAME_COLUMN]
+                if pid not in names:
+                    names[pid] = name
+                elif pid in names and names[pid] != name:
+                    raise ValueError(f'Repeat names for {pid}: {name}, {names[pid]}')
+                if pid in off_team:
+                    raise ValueError(f"Wait!! {pid} is in offense twice!!")
+                off_team[pid] = row["recent_team"]
+        with open(def_file, "rt") as infile:
             for row in csv.DictReader(infile):
-                if "def_games" not in row:
-                    raise ValueError("def_file missing def_games")
-                player_ids.add(row['player_id'])
-        self._player_ids = tuple(player_ids)
+                if row[_SEASON_TYPE_COLUMN] != season_type:
+                    continue
+                pid = row[_PID_COLUMN]
+                name = row[_NAME_COLUMN]
+                if pid not in def_teams:
+                    def_teams[pid] = {}
+                if pid not in names:
+                    names[pid] = name
+                elif pid in names and names[pid] != name:
+                    raise ValueError(f'Repeat names for {pid}: {name}, {names[pid]}')
+                team = row["team"]
+                num_games = int(row["def_games"])
+                def_teams[pid][team] = def_teams[pid].get(team, 0) + num_games
+        self._player_ids = tuple(
+            set(list(kck_teams.keys()) + 
+                list(off_team.keys()) + 
+                list(def_teams.keys())))
+        self._kckteams = kck_teams
+        self._offteams = off_team
+        self._defteams = def_teams
+        self._names = names
 
     @property
     def player_ids(self):
@@ -44,20 +85,11 @@ class SeasonStats:
 
 
 def main():
-    print('2022:')
     s22 = SeasonStats(_SEASON_2022, "REG+POST")
-    print('\n\n2022:')
-    s23 = SeasonStats(_SEASON_2023, "REG")
-    p22 = set(s22.player_ids)
-    p23 = set(s23.player_ids)
-    both = p22.intersection(p23)
-    either = p22.union(p23)
-    print(f"\n'22: {len(p22)}")
-    print(f"'23: {len(p23)}")
-    print(f"Both: {len(both)}")
-    print(f"Either: {len(either)}")
-    print(f"Just '22: {len(p22) - len(both)}")
-    print(f"Just '23: {len(p23) - len(both)}")
+    for pid in s22.player_ids:
+        if random.random() < 0.03:
+            print(f'{s22._names[pid]} ({pid}):\n\t{s22._offteams.get(pid, "[none]")}\n\t{s22._defteams.get(pid, {})}\n\t{s22._kckteams.get(pid, {})}\n')
+
 
 
 if __name__ == "__main__":
